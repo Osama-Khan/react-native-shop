d645741054156d4443344a480c90dd42cbe4b48f
Osama Khan <Osama6756@gmail.com>
2021-08-02 09:59:04 +0500

    Added basic products list to explore component

--- src/app/explore/explore.tsx
+++ src/app/explore/explore.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import {ScrollView} from 'react-native';
+import {ProgressBar} from 'react-native-paper';
+import ProductListingCard from '../components/product/product-listing-card';
+import Criteria from '../models/criteria';
+import productService from '../services/product.service';
 
-export default class Explore extends React.Component {
+export default class Explore extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {products: undefined};
+  }
+  componentDidMount() {
+    const criteria = new Criteria<any>();
+    criteria.addRelation('images');
+    productService.fetchProducts(criteria).then((res: any) => {
+      this.setState({...this.state, products: res.data.data});
+    });
+  }
+
   render() {
-    return <ScrollView></ScrollView>;
+    const products = this.state.products?.map((p: any) => (
+      <ProductListingCard product={p} />
+    )) || <ProgressBar indeterminate={true} />;
+    return <ScrollView>{products}</ScrollView>;
   }
 }
