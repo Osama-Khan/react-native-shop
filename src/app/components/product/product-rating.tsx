3039d0b8f7dee3bbf07326f0855ebb550e14d2f2
Osama Khan <Osama6756@gmail.com>
2021-08-05 01:08:30 +0500

    Added ProductRating to show rating in star icons

---
+++ src/app/components/product/product-rating.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {ViewProps} from 'react-native';
+import {View} from 'react-native';
+import colors from '../../../styles/colors';
+import s from '../../../styles/styles';
+import Icon from '../icon';
+
+type PropType = ViewProps & {rating: number; iconSize?: number};
+export default ({rating, iconSize, ...props}: PropType) => {
+  const starsEl = [];
+  const color = rating ? colors.yellow : colors.gray;
+  const iProps = {color, size: iconSize};
+
+  if (rating > 0) {
+    let iters,
+      count = 0;
+    for (iters = rating; iters >= 1; iters--) {
+      starsEl.push(<Icon key={count} name="star" {...iProps} />);
+      count++;
+    }
+    if (iters !== 0) {
+      starsEl.push(<Icon key={count} name="star-half-full" {...iProps} />);
+      count++;
+    }
+    while (count < 5) {
+      starsEl.push(<Icon key={count} name="star-outline" {...iProps} />);
+      count++;
+    }
+  } else {
+    for (let i = 0; i < 5; i++) {
+      starsEl.push(<Icon key={i} name="star-outline" {...iProps} />);
+    }
+  }
+
+  return (
+    <View {...props} style={[s.row, [props.style || {}]]}>
+      {starsEl}
+    </View>
+  );
+};
