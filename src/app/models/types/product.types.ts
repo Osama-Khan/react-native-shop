bb9a452c5190a5665a0b0ebd46f914b4930b0abc
Osama Khan <Osama6756@gmail.com>
2021-08-05 07:13:04 +0500

    Added a rating field in ProductType
    This field is used when the API is returning list of products. By default, the API attaches an average of all ratings as rating property to the response data.

--- src/app/models/types/product.types.ts
+++ src/app/models/types/product.types.ts
@@ -23,6 +23,7 @@ export type ProductType = EntityType & {
   price: number;
   highlights?: HighlightType[];
   images?: ImageType[];
+  rating?: number;
   ratings?: RatingType[];
   favorites?: FavoriteType[];
   category?: CategoryType;
