4eb9a3057d8322324e3b45e4e551869589570ffc
Osama Khan <Osama6756@gmail.com>
2021-08-06 13:16:25 +0500

    Separated and improved login and profile components

---
+++ src/app/account/login.tsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import {StyleSheet, ToastAndroid, View} from 'react-native';
+import {
+  Title,
+  Text,
+  Divider,
+  TextInput,
+  Switch,
+  Button,
+} from 'react-native-paper';
+import colors from '../../styles/colors';
+import defaultStyles from '../../styles/styles';
+import userService from '../services/user.service';
+import appState from '../state/state';
+
+type PropType = {onLogin: () => void};
+type StateType = {
+  username: string;
+  password: string;
+  remember: boolean;
+  loading: boolean;
+};
+
+export default class extends React.Component<PropType, StateType> {
+  passwordRef: any;
+
+  constructor(props: PropType) {
+    super(props);
+    this.state = {username: '', password: '', remember: false, loading: false};
+    this.passwordRef = React.createRef();
+  }
+
+  render() {
+    return (
+      <View style={styles.loginForm}>
+        <Title style={styles.textCenter}>Login</Title>
+        <Text style={[styles.textCenter, styles.textMuted]}>
+          Please enter your account details.
+        </Text>
+        <Divider style={styles.mt8} />
+        <TextInput
+          label="Username"
+          style={styles.mt8}
+          value={this.state.username}
+          mode="outlined"
+          onSubmitEditing={() => this.passwordRef.current.focus()}
+          onChangeText={username =>
+            this.setState({
+              ...this.state,
+              username,
+            })
+          }
+        />
+        <TextInput
+          label="Password"
+          style={styles.mt8}
+          value={this.state.password}
+          mode="outlined"
+          secureTextEntry={true}
+          onSubmitEditing={this.login}
+          ref={this.passwordRef}
+          onChangeText={password =>
+            this.setState({
+              ...this.state,
+              password,
+            })
+          }
+        />
+        <View style={[styles.m4, styles.row]}>
+          <Text style={[styles.m4, styles.mlAuto]}>Remember me</Text>
+          <Switch
+            value={this.state.remember}
+            onValueChange={remember =>
+              this.setState({
+                ...this.state,
+                remember,
+              })
+            }
+          />
+        </View>
+        <Button style={styles.mlAuto}>Forgot Password?</Button>
+        <Button
+          mode="contained"
+          loading={this.state.loading}
+          disabled={this.state.loading}
+          style={styles.mt8}
+          color={colors.primary}
+          onPress={this.login}>
+          Login
+        </Button>
+      </View>
+    );
+  }
+  login = () => {
+    this.setState({...this.state, loading: true});
+    const {username, password} = this.state;
+    if (!username || !password) {
+      ToastAndroid.show('Please fill in both fields.', ToastAndroid.SHORT);
+      return;
+    }
+
+    userService.login(username, password).then(u => {
+      appState.user = u.data;
+      this.setState({...this.state, loading: false});
+      this.props.onLogin();
+    });
+  };
+}
+const styles = StyleSheet.create({
+  ...defaultStyles,
+  loginForm: {
+    margin: 8,
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
---
+++ src/app/account/profile.tsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import {ScrollView, Image, View} from 'react-native';
+import {Title, Text, Badge, Divider, List} from 'react-native-paper';
+import colors from '../../styles/colors';
+import appStyles from '../../styles/styles';
+import appState from '../state/state';
+
+export default ({onLogout}: {onLogout: () => void}) => {
+  return (
+    <ScrollView>
+      <View style={[styles.m4, styles.row]}>
+        <Image
+          source={{uri: appState.user.profileImage}}
+          resizeMode="contain"
+          style={styles.profileImage}
+        />
+        <View style={styles.headerContent}>
+          <Title>
+            {appState.user.firstName} {appState.user.lastName}
+          </Title>
+          <Text style={styles.textMuted}>{'@' + appState.user.username}</Text>
+          <View style={styles.row}>
+            <Badge style={[styles.mt8, {backgroundColor: colors.primary}]}>
+              {appState.user.roles && appState.user.roles[0].name}
+            </Badge>
+          </View>
+        </View>
+      </View>
+      <Divider />
+      <List.Section title="Overview">
+        <List.Item
+          title="Listings"
+          description="Manage your products"
+          onPress={() => {}}
+          left={() => <List.Icon icon="clipboard-list" />}
+        />
+        <List.Item
+          title="Orders"
+          description="View your orders"
+          onPress={() => {}}
+          left={() => <List.Icon icon="receipt" />}
+        />
+        <List.Item
+          title="Favorites"
+          description="See your favorite products"
+          onPress={() => {}}
+          left={() => <List.Icon icon="star" />}
+        />
+      </List.Section>
+      <Divider />
+      <List.Section title="Management">
+        <List.Item
+          title="Account"
+          description="Update your account information"
+          onPress={() => {}}
+          left={() => <List.Icon icon="account-edit" />}
+        />
+        <List.Item
+          title="Addresses"
+          description="Manage your addresses"
+          onPress={() => {}}
+          left={() => <List.Icon icon="home-edit" />}
+        />
+        <Divider />
+      </List.Section>
+      {appState.user.roles?.some(r => r.name === 'admin') ? (
+        <List.Section title="Admin Panel">
+          <List.Item
+            title="Users"
+            description="Manage Users"
+            onPress={() => {}}
+            left={() => <List.Icon icon="account-group" />}
+          />
+          <List.Item
+            title="Products"
+            description="Manage Products"
+            onPress={() => {}}
+            left={() => <List.Icon icon="archive" />}
+          />
+          <List.Item
+            title="Categories"
+            description="Manage Categories"
+            onPress={() => {}}
+            left={() => <List.Icon icon="shape" />}
+          />
+          <List.Item
+            title="Orders"
+            description="Manage Orders"
+            onPress={() => {}}
+            left={() => <List.Icon icon="receipt" />}
+          />
+        </List.Section>
+      ) : (
+        <></>
+      )}
+      <Divider />
+      <List.Item
+        title="Logout"
+        titleStyle={{color: colors.red}}
+        onPress={onLogout}
+        left={() => <List.Icon icon="power" color={colors.red} />}
+      />
+    </ScrollView>
+  );
+};
+
+const styles = {
+  ...appStyles,
+  profileImage: {
+    width: '25%',
+    minHeight: 92,
+    height: 'auto',
+  },
+  headerContent: {
+    width: '70%',
+    marginLeft: 'auto',
+  },
+};
