26d42f7bf480918726dffb3fb0712486a4062ed5
Osama Khan <Osama6756@gmail.com>
2021-08-07 11:19:12 +0500

    Added toast method

--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -1,6 +1,11 @@
+import {ToastAndroid} from 'react-native';
+
 /**
  * Service that provides methods related to UI feedback
  */
-class UiService {}
+class UiService {
+  toast = (msg: string, short = true) =>
+    ToastAndroid.show(msg, short ? ToastAndroid.SHORT : ToastAndroid.LONG);
+}
 
 export default new UiService();
