b84dd99a5b0e722171488a4cfafc4c5cca232f06
Osama Khan <Osama6756@gmail.com>
2021-08-12 17:44:44 +0500

    Added filtering and improved criteria implementation

--- src/app/search/search.tsx
+++ src/app/search/search.tsx
@@ -1,7 +1,7 @@
 import {NavigationProp} from '@react-navigation/native';
 import React from 'react';
 import {View} from 'react-native';
-import {Searchbar, Text} from 'react-native-paper';
+import {FAB, Searchbar, Text} from 'react-native-paper';
 import s from '../../styles/styles';
 import Criteria from '../models/criteria';
 import {ProductType} from '../models/types/product.types';
@@ -9,9 +9,14 @@ import Icon from '../components/icon';
 import colors from '../../styles/colors';
 import {createRef} from 'react';
 import ProductListing from '../components/product/product-listing';
+import ProductFiltersModal from '../components/product/product-filters-modal/product-filters-modal';
 
 type PropType = {navigation: NavigationProp<any>};
-type StateType = {criteria?: Criteria<ProductType>; query: string};
+type StateType = {
+  criteria?: Criteria<ProductType>;
+  query: string;
+  showFilters: boolean;
+};
 export default class Search extends React.Component<PropType, StateType> {
   readonly updateDelay = 500;
   updateTimeout?: NodeJS.Timeout;
@@ -19,7 +24,7 @@ export default class Search extends React.Component<PropType, StateType> {
 
   constructor(props: PropType) {
     super(props);
-    this.state = {query: ''};
+    this.state = {query: '', showFilters: false};
     this.searchBarRef = createRef();
     props.navigation.addListener('focus', () => {
       this.searchBarRef.current?.focus();
@@ -27,14 +32,16 @@ export default class Search extends React.Component<PropType, StateType> {
   }
 
   render() {
+    const filters = this.state.showFilters;
     return (
       <View style={s.flex}>
         <Searchbar
-          style={[s.m4]}
+          style={[s.roundedNone]}
           value={this.state.query}
           placeholder="Search Products..."
           onChangeText={this.handleSearchChange}
           ref={this.searchBarRef}
+          editable={!filters}
         />
         {this.state.criteria ? (
           <ProductListing
@@ -49,6 +56,34 @@ export default class Search extends React.Component<PropType, StateType> {
             </Text>
           </View>
         )}
+        <FAB
+          icon="filter"
+          style={[s.bottomRight, s.m8]}
+          onPress={() => this.setState({...this.state, showFilters: true})}
+          disabled={filters}
+        />
+        <ProductFiltersModal
+          visible={filters}
+          onApply={state => {
+            const criteria = new Criteria<ProductType>();
+            criteria.setOrderBy(state.sortBy);
+            criteria.setOrderDir(state.sortByOrder);
+            if (state.maxPrice < 100000) {
+              criteria.addFilter('price', state.maxPrice, '<=');
+            }
+            if (state.outOfStock === false) {
+              criteria.addFilter('stock', 0, '>');
+            }
+            this.addSearchQuery(criteria);
+            this.setState({...this.state, criteria, showFilters: false});
+          }}
+          onClear={() => {
+            const criteria = new Criteria<ProductType>();
+            this.addSearchQuery(criteria);
+            this.setState({...this.state, criteria, showFilters: false});
+          }}
+          onDismiss={() => this.setState({...this.state, showFilters: false})}
+        />
       </View>
     );
   }
@@ -68,13 +103,14 @@ export default class Search extends React.Component<PropType, StateType> {
     // Set a timeout to update criteria
     this.updateTimeout = setTimeout(() => {
       const criteria = this.state.query
-        ? new Criteria<ProductType>()
+        ? new Criteria<ProductType>(this.state.criteria)
         : undefined;
-      const query = `%${this.state.query}%`;
-      criteria?.addFilter('title', query, 'like');
-      criteria?.addRelation('images');
+      this.addSearchQuery(criteria);
       this.setState({...this.state, criteria});
       this.updateTimeout = undefined;
     }, this.updateDelay);
   };
+
+  addSearchQuery = (criteria?: Criteria<ProductType>) =>
+    criteria?.addFilter('title', `%25${this.state.query}%25`, 'like');
 }
