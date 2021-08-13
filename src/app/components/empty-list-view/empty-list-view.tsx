5a3570fc89469950d2ba6eb24446a46f16b0173b
Osama Khan <Osama6756@gmail.com>
2021-08-13 14:04:56 +0500

    Added EmptyListView to be shown when a view has no data

---
+++ src/app/components/empty-list-view/empty-list-view.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Button, Caption, Title} from 'react-native-paper';
+import colors from '../../../styles/colors';
+import s from '../../../styles/styles';
+import Icon from '../icon';
+
+type ButtonProps = {text: string; icon: string; action: () => void};
+type PropType = {
+  icon: string;
+  title: string;
+  caption: string;
+  btnProps: ButtonProps;
+};
+
+/** A standard view to show in case no data is found for a list */
+export default class EmptyListView extends React.Component<PropType> {
+  render() {
+    return (
+      <View style={[s.flex, s.center]}>
+        <Icon name={this.props.icon} size={64} color={colors.gray} />
+        <Title>{this.props.title}</Title>
+        <Caption>{this.props.caption}</Caption>
+        <Button
+          style={s.my8}
+          mode="contained"
+          icon={this.props.btnProps.icon}
+          onPress={this.props.btnProps.action}>
+          {this.props.btnProps.text}
+        </Button>
+      </View>
+    );
+  }
+}
