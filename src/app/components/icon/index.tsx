57486b78f971823c8ec5930648841ebb0914899a
Osama Khan <Osama6756@gmail.com>
2021-08-13 14:08:21 +0500

    Improved icon to use color based on current theme

--- src/app/components/icon/index.tsx
+++ src/app/components/icon/index.tsx
@@ -1,3 +1,11 @@
+import React from 'react';
+import {IconProps} from 'react-native-vector-icons/Icon';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import colors from '../../../styles/colors';
+import themeService from '../../services/theme.service';
 
-export default Icon;
+export default (props: IconProps) => {
+  const theme = themeService.getCurrentTheme();
+  const color = theme === 'dark' ? colors.white : colors.dark;
+  return <Icon {...props} color={props.color || color} />;
+};
