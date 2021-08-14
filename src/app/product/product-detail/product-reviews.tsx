66e98a354788865eabc9cfb5a149fd563772076f
Osama Khan <Osama6756@gmail.com>
2021-08-14 14:47:45 +0500

    Implemented user review feature

--- src/app/product/product-detail/product-reviews.tsx
+++ src/app/product/product-detail/product-reviews.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Icon from '../../components/icon/';
-import {Image, View} from 'react-native';
-import {Button, Card, ProgressBar, Text, Title} from 'react-native-paper';
+import {View} from 'react-native';
+import {Button, Caption, ProgressBar, Title} from 'react-native-paper';
 import s from '../../../styles/styles';
 import {ProductType, RatingType} from '../../models/types/product.types';
 import productService from '../../services/product.service';
 import Criteria from '../../models/criteria';
-import colors from '../../../styles/colors';
-import Rating from '../../components/product/product-rating';
+import appState from '../../state/state';
+import OwnReview from './product-review-own';
+import ReviewCard from './product-review-card';
 
 type PropType = {product: ProductType};
 type StateType = {
@@ -37,44 +38,40 @@ export default class extends React.Component<PropType, StateType> {
         <Title>
           <Icon name="account-star" size={22} /> User Reviews
         </Title>
-        {this.state.reviews ? (
+        {appState.user.token ? (
           <>
-            {this.state.reviews.map(r => (
-              <Card key={r.id} style={[s.p4, s.m4]}>
-                <View style={s.row}>
-                  <View style={[s.col3, s.center]}>
-                    <Image
-                      source={{
-                        uri: r.user!.profileImage,
-                        height: 48,
-                        width: 48,
-                      }}
-                      style={s.rounded}
-                    />
-                    <Text>@{r.user!.username}</Text>
-                  </View>
-                  <View style={[s.col9, s.p4]}>
-                    <Rating rating={r.stars} />
-                    <Text style={[s.textBold]}>{r.title || 'No Title'}</Text>
-                    <Text>{r.description || 'No Description'}</Text>
-                  </View>
-                </View>
-              </Card>
-            ))}
-            {this.state.loading ? (
-              <this.Progress />
-            ) : this.state.meta.currentPage < this.state.meta.totalPages ? (
-              <Button
-                onPress={() => {
-                  this.state.criteria.setPage(++this.page);
-                  this.fetchReviews();
-                }}>
-                More
-              </Button>
-            ) : (
-              <Button color={colors.gray}>No more reviews</Button>
-            )}
+            <Caption>Your review</Caption>
+            <OwnReview productId={this.props.product.id} />
+            <Caption>Other reviews</Caption>
           </>
+        ) : (
+          <></>
+        )}
+        {this.state.reviews ? (
+          this.state.reviews.length > 0 ? (
+            <>
+              {this.state.reviews.map(r => (
+                <ReviewCard key={r.id} review={r} />
+              ))}
+              {this.state.loading ? (
+                <this.Progress />
+              ) : this.state.meta.currentPage < this.state.meta.totalPages ? (
+                <Button
+                  onPress={() => {
+                    this.state.criteria.setPage(++this.page);
+                    this.fetchReviews();
+                  }}>
+                  More
+                </Button>
+              ) : (
+                <Caption style={s.alignCenter}>No more reviews</Caption>
+              )}
+            </>
+          ) : (
+            <Caption style={s.alignCenter}>
+              No reviews found for this product
+            </Caption>
+          )
         ) : (
           <this.Progress />
         )}
@@ -86,6 +83,8 @@ export default class extends React.Component<PropType, StateType> {
 
   fetchReviews = () => {
     this.setState({...this.state, loading: true});
+    // Filter out own review
+    this.state.criteria.addFilter('user', appState.user.id, '!=');
     productService.getRatings(this.state.criteria).then(res => {
       let reviews = this.state.reviews || [];
       reviews = reviews.concat(res.data.data);
