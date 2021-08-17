5d3daa1bcdb49b5c055357b31923fc1b69800a8a
Osama Khan <Osama6756@gmail.com>
2021-08-17 14:47:11 +0500

    Improved UserLikes UI, Fixed some type issues in code

--- src/app/account/user-likes.tsx
+++ src/app/account/user-likes.tsx
@@ -1,7 +1,6 @@
 import {NavigationProp} from '@react-navigation/native';
 import React from 'react';
-import {View} from 'react-native';
-import {Card, IconButton, Text} from 'react-native-paper';
+import {IconButton, List} from 'react-native-paper';
 import s from '../../styles/styles';
 import {productDetailRoute} from '../app.routes';
 import ListingComponent from '../components/listing/listing';
@@ -11,6 +10,7 @@ import favoriteService from '../services/favorite.service';
 import appState from '../state/state';
 import colors from '../../styles/colors';
 import IconMessageView from '../components/icon-message-view/icon-message-view';
+import {FavoriteType} from '../models/types/favorite.type';
 
 type PropType = {navigation: NavigationProp<any>};
 type StateType = {unliked: number[]; unliking: number[]};
@@ -21,15 +21,20 @@ export default class UserLikes extends React.Component<PropType, StateType> {
     this.state = {unliked: [], unliking: []};
   }
   render() {
-    const criteria = new Criteria<ProductType>();
+    const criteria = new Criteria<FavoriteType>();
     criteria.addFilter('user', appState.user.id!);
     return (
       <ListingComponent
         container={product =>
+          /** Favorite and Product type collision occurs because
+           * ListingComponent expects request criteria type and
+           * response object type to be same. However, the endpoint
+           * used in getFavoritesOfUser method gets the criteria of
+           * FavoriteType but returns ProductType data. */
           this.state.unliked.includes(product.id) ? (
             <></>
           ) : (
-            <this.Card product={product} key={product.id} />
+            <this.Item product={product as ProductType} key={product.id} />
           )
         }
         fetchMethod={c =>
@@ -52,41 +57,34 @@ export default class UserLikes extends React.Component<PropType, StateType> {
     );
   }
 
-  Card = ({product}: {product: ProductType}) => (
-    <Card
+  Item = ({product}: {product: ProductType}) => (
+    <List.Item
       style={s.card}
       onPress={() =>
         this.props.navigation.navigate(productDetailRoute.name, {
           id: product.id,
         })
-      }>
-      <View style={s.row}>
-        <View style={[s.flex, s.m8]}>
-          <View>
-            <Text numberOfLines={2} style={s.textBold}>
-              {product.title}
-            </Text>
-          </View>
-        </View>
-        <View style={s.mlAuto}>
-          <IconButton
-            style={s.alignCenter}
-            icon="heart-off"
-            color={colors.red}
-            disabled={this.state.unliking.includes(product.id)}
-            onPress={() => {
-              const unliking = [...this.state.unliking, product.id];
-              this.setState({...this.state, unliking});
-              favoriteService
-                .unsetFavorite(appState.user.id!, product.id)
-                .then(() => {
-                  const unliked = [...this.state.unliked, product.id];
-                  this.setState({...this.state, unliked});
-                });
-            }}
-          />
-        </View>
-      </View>
-    </Card>
+      }
+      title={product.title}
+      description={product.description}
+      right={() => (
+        <IconButton
+          style={s.alignCenter}
+          icon="heart-off"
+          color={colors.red}
+          disabled={this.state.unliking.includes(product.id)}
+          onPress={() => {
+            const unliking = [...this.state.unliking, product.id];
+            this.setState({...this.state, unliking});
+            favoriteService
+              .unsetFavorite(appState.user.id!, product.id)
+              .then(() => {
+                const unliked = [...this.state.unliked, product.id];
+                this.setState({...this.state, unliked});
+              });
+          }}
+        />
+      )}
+    />
   );
 }
