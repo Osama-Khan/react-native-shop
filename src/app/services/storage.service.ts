da3e804e019acb73f11386d961ecea8f8c6f26fd
Osama Khan <Osama6756@gmail.com>
2021-08-19 17:51:28 +0500

    Added StorageService functionality

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,50 +1,57 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import CartState from '../state/cart-state';
 
 class StorageService {
-  userTokenKey = 'user-token';
+  tokenKey = 'user-token';
   cartKey = 'cart';
 
   /**
    * Saves the given token in localStorage
    * @param token The token to save
    */
-  saveUserToken = (token: string) => {
-    // TBI
-  };
+  saveUserToken = (token: string) => AsyncStorage.setItem(this.tokenKey, token);
 
   /**
    * Gets the user token from localStorage
    */
-  loadUserToken = () => {
-    // TBI
-  };
+  loadUserToken = () => AsyncStorage.getItem(this.tokenKey);
 
   /**
    * Removes the user-token from storage
    */
-  clearUserToken = () => {
-    // TBI
-  };
+  clearUserToken = () => AsyncStorage.removeItem(this.tokenKey);
 
   /**
    * Saves cart product IDs and Quantities in localStorage
    * @param cart The state of the cart
    */
   updateCart = (cart: CartState) => {
-    // TBI
+    if (cart.products?.length > 0) {
+      return AsyncStorage.setItem(
+        this.cartKey,
+        cart.products.map(p => p.id + ':' + p.quantity).join(';'),
+      );
+    }
+    return this.clearCart();
   };
 
   /** Removes cart data from localStorage */
-  clearCart = () => {
-    // TBI
-  };
+  clearCart = () => AsyncStorage.removeItem(this.cartKey);
 
   /**
    * Loads cart product IDs and Quantities saved on last session
    * @returns list of objects containing product id and quantity
    */
-  loadCart = () => {
-    // TBI
+  loadCart = async () => {
+    const v = await AsyncStorage.getItem(this.cartKey);
+    if (v) {
+      const items = v.split(';');
+      return items?.map(i => {
+        const data = i.split(':').map(n => parseInt(n, 10));
+        return {id: data[0], quantity: data[1]};
+      });
+    }
+    return [];
   };
 }
 
