7e52641fc1521d010118bf1ae8ba3b7d9a70eabb
Osama Khan <Osama6756@gmail.com>
2021-08-19 17:52:54 +0500

    Added fromJson method
    Allows to make any modifications necessary to API JSON response before storing data in UserState

--- src/app/state/user-state.ts
+++ src/app/state/user-state.ts
@@ -11,6 +11,15 @@ export default class UserState extends User implements IUserState {
 
   constructor(obj?: IUserState) {
     super(obj);
-    if (obj) this.token = obj.token;
+    if (obj) {
+      this.token = obj.token;
+    }
+  }
+
+  /** Initializes a User from server JSON response. */
+  static fromJson(data: any) {
+    const user = new UserState(data);
+    user.dateOfBirth = new Date(user.dateOfBirth!);
+    return user;
   }
 }
