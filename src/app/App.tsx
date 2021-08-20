9226bdc2c395f3083926e1fe771145302a9bd179
Osama Khan <Osama6756@gmail.com>
2021-08-20 19:22:02 +0500

    Added Redux Provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,6 +7,8 @@ import {Provider as PaperProvider} from 'react-native-paper';
 import {DarkTheme, DefaultTheme} from '../styles/themes/themes';
 import initializeInterceptors from './interceptors/interceptor-initializer';
 import restoreSession from './session/restore-session';
+import {Provider as ReduxProvider} from 'react-redux';
+import store from './store';
 
 const App = () => {
   const isDark = useColorScheme() === 'dark';
@@ -14,20 +16,22 @@ const App = () => {
   initializeInterceptors();
   restoreSession();
   return (
-    <PaperProvider theme={isDark ? DarkTheme : DefaultTheme}>
-      <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
-        <Stack.Navigator>
-          {routes.stackNav.map(r => (
-            <Stack.Screen
-              name={r.name}
-              key={r.id}
-              options={r.options}
-              component={r.component}
-            />
-          ))}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
+    <ReduxProvider store={store}>
+      <PaperProvider theme={isDark ? DarkTheme : DefaultTheme}>
+        <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
+          <Stack.Navigator>
+            {routes.stackNav.map(r => (
+              <Stack.Screen
+                name={r.name}
+                key={r.id}
+                options={r.options}
+                component={r.component}
+              />
+            ))}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </ReduxProvider>
   );
 };
 
