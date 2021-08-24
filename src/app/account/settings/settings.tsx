8084774643eb254b151bb9ce4f4ddea289f687af
Osama Khan <Osama6756@gmail.com>
2021-08-25 04:08:26 +0500

    Added functionality to switch theme

--- src/app/account/settings/settings.tsx
+++ src/app/account/settings/settings.tsx
@@ -3,8 +3,14 @@ import {View} from 'react-native';
 import {List} from 'react-native-paper';
 import s from '../../../styles/styles';
 import ListSelect from '../../components/list-select';
+import storageService from '../../services/storage.service';
+import themeService from '../../services/theme.service';
+import uiService from '../../services/ui.service';
 
 export default class Settings extends React.Component {
+  initialTheme = themeService.currentTheme;
+  initiallyDeviceTheme = themeService.isDeviceTheme;
+
   render() {
     return (
       <List.Section title="Appearance">
@@ -13,29 +19,44 @@ export default class Settings extends React.Component {
           description="Theme of the application"
           right={() => (
             <View style={s.alignCenter}>
-              <ListSelect onSelect={() => {}} options={themeOptions} />
+              <ListSelect
+                onSelect={v => {
+                  if (v) {
+                    storageService.setUserTheme(v.value as 'light' | 'dark');
+                  } else {
+                    storageService.clearUserTheme();
+                  }
+                  uiService.toast('Changes will be applied on restart!');
+                }}
+                options={this.themeOptions}
+              />
             </View>
           )}
         />
       </List.Section>
     );
   }
-}
 
-const themeOptions = [
-  {
-    name: 'Device Theme',
-    value: '',
-    icon: 'theme-light-dark',
-  },
-  {
-    name: 'Light Theme',
-    value: 'light',
-    icon: 'white-balance-sunny',
-  },
-  {
-    name: 'Dark Theme',
-    value: 'dark',
-    icon: 'weather-night',
-  },
-];
+  get themeOptions() {
+    return [
+      {
+        name: 'Device Theme',
+        value: '',
+        icon: 'theme-light-dark',
+        selected: this.initiallyDeviceTheme,
+      },
+      {
+        name: 'Light Theme',
+        value: 'light',
+        icon: 'white-balance-sunny',
+        selected: this.initialTheme === 'light',
+      },
+      {
+        name: 'Dark Theme',
+        value: 'dark',
+        icon: 'weather-night',
+        selected: this.initialTheme === 'dark',
+      },
+    ];
+  }
+}
