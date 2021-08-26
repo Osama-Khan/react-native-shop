b8abdf1b4a0eeb2bac47cabe5b22abca02557695
Osama Khan <Osama6756@gmail.com>
2021-08-26 20:48:00 +0500

    Improved Icon code

--- src/app/components/icon/index.tsx
+++ src/app/components/icon/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {IconProps} from 'react-native-vector-icons/Icon';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import colors from '../../../styles/colors';
 import themeService from '../../services/theme.service';
 
-export default (props: IconProps) => {
+export default function Icon(props: IconProps) {
   const theme = themeService.currentTheme;
   const color = theme === 'dark' ? colors.white : colors.dark;
-  return <Icon {...props} color={props.color || color} />;
-};
+  return <MaterialIcon {...props} color={props.color || color} />;
+}
