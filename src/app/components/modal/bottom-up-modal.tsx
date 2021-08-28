97f70fb039f95d1580b99707fcbde8edf0861885
Osama Khan <Osama6756@gmail.com>
2021-08-28 17:14:44 +0500

    Replaced custom gradient with LinearGradient component

--- src/app/components/modal/bottom-up-modal.tsx
+++ src/app/components/modal/bottom-up-modal.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {ScrollView} from 'react-native-gesture-handler';
-import Svg, {Defs, LinearGradient, Rect, Stop} from 'react-native-svg';
+import Svg, {Defs, Rect, Stop} from 'react-native-svg';
 import colors from '../../../styles/colors';
 import s from '../../../styles/styles';
+import LinearGradient from '../gradient/linear-gradient';
 import Icon from '../icon';
 import ModalBase from './modal-base';
 import {ModalPropType} from './modal-prop-type';
@@ -23,16 +24,6 @@ export default function BottomUpModal(props: ModalPropType) {
   );
 }
 
-const bgGradient = (
-  <Svg width="100%" height="100%">
-    <Defs>
-      <LinearGradient id="grad" x1="0" y1="1" x2="0" y2="0">
-        <Stop offset="0" stopColor="#000" stopOpacity="1" />
-        <Stop offset="1" stopColor="#0000" stopOpacity="0" />
-      </LinearGradient>
-    </Defs>
-    <Rect width="100%" height="100%" x="0" y="0" fill="url(#grad)" />
-  </Svg>
-);
+const bgGradient = <LinearGradient start="bottom" end="top" />;
 
 const modalStyle = {maxHeight: '80%'};
