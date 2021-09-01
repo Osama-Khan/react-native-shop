3b74a6741ffba2e60f49b11444d4965b79e75f43
Osama Khan <Osama6756@gmail.com>
2021-09-01 17:15:46 +0500

    Added clear cart button

--- src/app/cart/cart.tsx
+++ src/app/cart/cart.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Image} from 'react-native';
+import {View, Image, Alert, AlertButton} from 'react-native';
 import {
   Card,
   Divider,
@@ -8,6 +8,7 @@ import {
   Surface,
   FAB,
   Title,
+  IconButton,
 } from 'react-native-paper';
 import s from '../../styles/styles';
 import CartProduct from '../models/product/cart-product';
@@ -17,6 +18,8 @@ import {checkoutRoute, productDetailRoute} from '../app.routes';
 import IconMessageView from '../components/icon-message-view/icon-message-view';
 import {connect} from 'react-redux';
 import {AppStateType} from '../store/state';
+import colors from '../../styles/colors';
+import cartActions from '../store/actions/cart.actions';
 
 class Cart extends React.Component<any, any> {
   render() {
@@ -40,6 +43,29 @@ class Cart extends React.Component<any, any> {
           <>
             <ScrollView>
               <List.Section title="Products">
+                <IconButton
+                  style={[s.topRight, s.m8]}
+                  icon="trash-can"
+                  color={colors.red}
+                  onPress={() => {
+                    const btnYes: AlertButton = {
+                      text: 'Yes',
+                      style: 'destructive',
+                      onPress: () => {
+                        this.props.dispatch(cartActions.clearCart());
+                      },
+                    };
+                    const btnNo: AlertButton = {
+                      text: 'No',
+                      style: 'cancel',
+                    };
+                    const title = 'Clear cart?';
+                    const msg = 'Are you sure you want to clear the cart?';
+                    Alert.alert(title, msg, [btnYes, btnNo], {
+                      cancelable: true,
+                    });
+                  }}
+                />
                 {products.map(p => (
                   <this.CartProduct key={p.id} product={p} />
                 ))}
