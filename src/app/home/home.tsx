8881a6bdc37708fbcb4eda691a7513e4697c4574
Osama Khan <Osama6756@gmail.com>
2021-09-02 20:17:19 +0500

    Added a SplashScreen

--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -11,82 +11,88 @@ import themeService from '../services/theme.service';
 import Triangle from '../components/svg/triangle';
 import AnimatedSearchbar from './animated-searchbar';
 import {createAnimatableComponent} from 'react-native-animatable';
+import SplashScreen from './splash-screen';
 
 type P = {navigation: NavigationProp<any>; readonly user: UserState};
-
 class Home extends React.Component<P> {
   hiGuyImage: any;
 
   render() {
     const AnimatedImage = createAnimatableComponent(Image);
     return (
-      <ScrollView>
-        <View style={s.p8}>
-          <Title style={s.textBold}>
-            Hello
-            {this.props.user.firstName ? ', ' + this.props.user.firstName : ''}!
-          </Title>
-          <Caption>What would you like to Shop today?</Caption>
-        </View>
-        <AnimatedImage
-          style={[s.topRight, s.mr24, s.mt4, {width: 84, height: 204}]}
-          source={require('../../assets/images/character/hi.png')}
-          resizeMode="contain"
-          ref={ref => (this.hiGuyImage = ref)}
-        />
-        <AnimatedSearchbar
-          onSearch={withSearch =>
-            this.props.navigation.navigate('Search', {withSearch})
-          }
-          onFocus={() => {
-            this.hiGuyImage.lightSpeedOut(500);
-          }}
-          onBlur={() => {
-            this.hiGuyImage.lightSpeedIn(500);
-          }}
-        />
-        <View style={s.mt8} />
-        <View style={[s.col12, {height: 24}]}>
-          <Triangle
-            color={
-              themeService.currentThemeName === 'dark'
-                ? themeService.currentTheme.colors.border
-                : themeService.currentTheme.colors.surface
-            }
-          />
-        </View>
-        <Surface>
-          <Title style={s.m8}>Browse Categories</Title>
-          <CategoriesList navigation={this.props.navigation} />
-          <Divider style={s.mt16} />
-          <Title style={s.m8}>New Arrivals</Title>
-          <NewArrivals navigation={this.props.navigation} />
-          <View style={[s.mt16, s.col12, {height: 24}]}>
-            <Triangle color={themeService.currentTheme.colors.background} />
+      <>
+        <SplashScreen />
+        <ScrollView>
+          <View style={s.p8}>
+            <Title style={s.textBold}>
+              Hello
+              {this.props.user.firstName
+                ? ', ' + this.props.user.firstName
+                : ''}
+              !
+            </Title>
+            <Caption>What would you like to Shop today?</Caption>
           </View>
-        </Surface>
-        <Image
-          source={require('../../assets/images/box.png')}
-          style={[s.bottomLeft, s.ml4, {width: 114, height: 120}]}
-        />
-        <Title style={s.textCenter}> Get in Touch</Title>
-        <View style={[s.row, s.alignCenter]}>
-          <IconButton icon="facebook" />
-          <IconButton icon="twitter" />
-          <IconButton icon="instagram" />
-          <IconButton icon="linkedin" />
-          <IconButton icon="email" />
-        </View>
-        <Divider />
-        <View style={s.center}>
-          <Title> Useful links</Title>
-          <Caption style={{textDecorationLine: 'underline'}}>Support</Caption>
-          <Caption style={{textDecorationLine: 'underline'}}>
-            Privacy Policy
-          </Caption>
-          <Caption style={{textDecorationLine: 'underline'}}>Website</Caption>
-        </View>
-      </ScrollView>
+          <AnimatedImage
+            style={[s.topRight, s.mr24, s.mt4, {width: 84, height: 204}]}
+            source={require('../../assets/images/character/hi.png')}
+            resizeMode="contain"
+            ref={ref => (this.hiGuyImage = ref)}
+          />
+          <AnimatedSearchbar
+            onSearch={withSearch =>
+              this.props.navigation.navigate('Search', {withSearch})
+            }
+            onFocus={() => {
+              this.hiGuyImage.lightSpeedOut(500);
+            }}
+            onBlur={() => {
+              this.hiGuyImage.lightSpeedIn(500);
+            }}
+          />
+          <View style={s.mt8} />
+          <View style={[s.col12, {height: 24}]}>
+            <Triangle
+              color={
+                themeService.currentThemeName === 'dark'
+                  ? themeService.currentTheme.colors.border
+                  : themeService.currentTheme.colors.surface
+              }
+            />
+          </View>
+          <Surface>
+            <Title style={s.m8}>Browse Categories</Title>
+            <CategoriesList navigation={this.props.navigation} />
+            <Divider style={s.mt16} />
+            <Title style={s.m8}>New Arrivals</Title>
+            <NewArrivals navigation={this.props.navigation} />
+            <View style={[s.mt16, s.col12, {height: 24}]}>
+              <Triangle color={themeService.currentTheme.colors.background} />
+            </View>
+          </Surface>
+          <Image
+            source={require('../../assets/images/box.png')}
+            style={[s.bottomLeft, s.ml4, {width: 114, height: 120}]}
+          />
+          <Title style={s.textCenter}> Get in Touch</Title>
+          <View style={[s.row, s.alignCenter]}>
+            <IconButton icon="facebook" />
+            <IconButton icon="twitter" />
+            <IconButton icon="instagram" />
+            <IconButton icon="linkedin" />
+            <IconButton icon="email" />
+          </View>
+          <Divider />
+          <View style={s.center}>
+            <Title> Useful links</Title>
+            <Caption style={{textDecorationLine: 'underline'}}>Support</Caption>
+            <Caption style={{textDecorationLine: 'underline'}}>
+              Privacy Policy
+            </Caption>
+            <Caption style={{textDecorationLine: 'underline'}}>Website</Caption>
+          </View>
+        </ScrollView>
+      </>
     );
   }
 }
---
+++ src/app/home/splash-screen.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Modal, View} from 'react-native';
+import {Caption, Title} from 'react-native-paper';
+import colors from '../../styles/colors';
+import Logo from '../components/svg/logo';
+import {createAnimatableComponent} from 'react-native-animatable';
+
+type P = {duration?: number};
+type S = {visible: boolean};
+export default class SplashScreen extends React.Component<P, S> {
+  state = {visible: true};
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({visible: false});
+    }, this.props.duration || 3000);
+  }
+
+  render() {
+    const AnimView = createAnimatableComponent(View);
+    return (
+      <Modal
+        animationType="fade"
+        visible={this.state.visible}
+        statusBarTranslucent={true}>
+        <AnimView
+          style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+          animation="zoomIn">
+          <Logo size={92} />
+          <Title style={{color: colors.primary}}>ShopNative</Title>
+          <Caption>One stop shop, literally.</Caption>
+        </AnimView>
+      </Modal>
+    );
+  }
+}
