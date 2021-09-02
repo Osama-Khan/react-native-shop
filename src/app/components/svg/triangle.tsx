6fc5f436772ceb0616242f3fcaded149b128696c
Osama Khan <Osama6756@gmail.com>
2021-09-02 20:16:10 +0500

    Improved Triangle SVG Component
    - Switched to Polygon points instead of rotated rect clipping
    - Added responsiveness in case user window dimension changes
    - Added custom width specification through prop

--- src/app/components/svg/triangle.tsx
+++ src/app/components/svg/triangle.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import Svg, {Color, Rect} from 'react-native-svg';
+import {useState} from 'react';
+import {useEffect} from 'react';
+import {Dimensions} from 'react-native';
+import Svg, {Color, Polygon, Rect} from 'react-native-svg';
 
-type P = {color: Color; height?: number};
+type P = {color: Color; height?: number; width?: number};
 
-export default function Triangle({color, height}: P) {
+export default function Triangle({color, height, width: propWidth}: P) {
   height = height || 24;
+  if (propWidth) {
+    return <TrianglePolygon color={color} height={height} width={propWidth} />;
+  }
+
+  const [width, setWidth] = useState(Dimensions.get('window').width);
+
+  useEffect(() => {
+    Dimensions.addEventListener('change', ({window: {width: w}}) => {
+      setWidth(w);
+    });
+  });
+
+  return <TrianglePolygon color={color} height={height} width={width} />;
+}
+
+function TrianglePolygon({color, height, width}: P) {
+  const points = `0,0 ${width},${height} 0,${height} 0,0`;
   return (
     <Svg width="100%" height="100%">
-      <Rect width="100%" height="100%" rotation={height / 8} fill={color} />
+      <Polygon points={points} fill={color} />
     </Svg>
   );
 }
