3fd03bc2c969cfefdd9753629c9d2fbad483dfe8
Osama Khan <Osama6756@gmail.com>
2021-09-03 05:54:42 +0500

    Replaced custom slideshow component with react-native-swiper

--- src/app/product/product-detail/product-image-slideshow.tsx
+++ src/app/product/product-detail/product-image-slideshow.tsx
@@ -1,75 +1,39 @@
 import React from 'react';
 import {Image, View} from 'react-native';
-import {IconButton, Surface} from 'react-native-paper';
+import Swiper from 'react-native-swiper';
 import colors from '../../../styles/colors';
 import s from '../../../styles/styles';
-import Icon from '../../components/icon';
 import {ProductType} from '../../models/types/product.types';
 import ProductLikeAction from './product-like-action';
 
 type S = {activeIndex: number};
 type P = {product: ProductType; userId: number};
 
+const imageHeight = 240;
+
 export default class ProductImageSlideshow extends React.Component<P, S> {
   state = {activeIndex: 0};
   render() {
     const p = this.props.product;
-    const showControls = p.images!.length > 1;
+    const images = p.images!.map(i => (
+      <Image
+        key={i.id}
+        source={{
+          uri: i.image,
+          height: imageHeight,
+        }}
+        resizeMode="contain"
+      />
+    ));
+
     return (
       <View style={{backgroundColor: colors.white}}>
-        <Image
-          source={{
-            uri: p.images![this.state.activeIndex].image,
-            height: 240,
-          }}
-          resizeMode="contain"
-        />
-        {showControls ? (
-          <>
-            <IconButton
-              style={[s.left, {marginTop: 100}]}
-              onPress={() => {
-                let activeIndex = this.state.activeIndex;
-                if (--activeIndex < 0) {
-                  activeIndex = p.images!.length - 1;
-                }
-                this.setState({...this.state, activeIndex});
-              }}
-              icon="arrow-left-circle"
-              color={colors.dark}
-            />
-            <IconButton
-              style={[s.right, {marginTop: 100}]}
-              onPress={() => {
-                let activeIndex = this.state.activeIndex;
-                if (++activeIndex >= p.images!.length) {
-                  activeIndex = 0;
-                }
-                this.setState({...this.state, activeIndex});
-              }}
-              icon="arrow-right-circle"
-              color={colors.dark}
-            />
-            <View style={[s.bottom, s.row, s.center, s.col12]}>
-              {p.images?.map((img, i) => (
-                <Icon
-                  key={img.id}
-                  name={
-                    this.state.activeIndex === i ? 'circle' : 'circle-outline'
-                  }
-                  onPress={() => this.setState({...this.state, activeIndex: i})}
-                  color={colors.dark}
-                />
-              ))}
-            </View>
-          </>
-        ) : (
-          <></>
-        )}
-
+        <Swiper height={imageHeight} activeDotColor={colors.primary}>
+          {images}
+        </Swiper>
         {this.props.userId ? (
           <ProductLikeAction
-            style={s.bottomRight}
+            style={s.topRight}
             productId={p.id}
             userId={this.props.userId}
           />
