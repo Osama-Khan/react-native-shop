eebaded81faffb9fc1f3a34507ea9872f7c3f6f0
Osama Khan <Osama6756@gmail.com>
2021-09-07 17:23:28 +0500

    Improved Logo

--- src/app/components/svg/logo.tsx
+++ src/app/components/svg/logo.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
-import Svg, {Circle, Rect, Text} from 'react-native-svg';
+import Svg, {Circle, ClipPath, Defs, Rect, Text} from 'react-native-svg';
 import appVariables from '../../../styles/abstract/app.variables';
 import colors from '../../../styles/colors';
 
 export default function Logo({size}: {size: number}) {
   return (
     <Svg width={size} height={size}>
+      <Defs>
+        <ClipPath id="clip">
+          <Rect
+            x="10%"
+            y="45%"
+            width="80%"
+            height="50%"
+            rx={appVariables.borderRadius}
+            ry={appVariables.borderRadius}
+          />
+        </ClipPath>
+      </Defs>
       <Circle
         cx="50%"
-        cy="35%"
-        r="28%"
+        cy="40%"
+        r="30%"
         stroke={colors.primaryDark}
         strokeWidth="6"
       />
       <Rect
         x="10%"
-        y="40%"
+        y="38%"
         width="80%"
-        height="60%"
+        height="58%"
+        rx={appVariables.borderRadius}
+        ry={appVariables.borderRadius}
+        fill={colors.primaryDark}
+      />
+      <Rect
+        x="10%"
+        y="42%"
+        width="80%"
+        height="58%"
         rx={appVariables.borderRadius}
         ry={appVariables.borderRadius}
         fill={colors.primary}
@@ -26,18 +47,10 @@ export default function Logo({size}: {size: number}) {
         cx="50%"
         cy="40%"
         r="30%"
-        stroke={colors.primary}
-        strokeWidth="6"
-      />
-      <Text
-        x="35%"
-        y="85%"
-        fill={colors.white}
         stroke={colors.primaryDark}
-        fontSize={size / 2}
-        fontWeight="bold">
-        S
-      </Text>
+        strokeWidth="6"
+        clipPath="url(#clip)"
+      />
     </Svg>
   );
 }
