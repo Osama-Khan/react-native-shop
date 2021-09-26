92a66e0dad249b521426b18d4e8f8d7629c4ab60
Osama Khan <Osama6756@gmail.com>
2021-09-26 11:57:29 +0500

    Added datePickerProps prop

--- src/app/components/pickers/date-picker-input.tsx
+++ src/app/components/pickers/date-picker-input.tsx
@@ -2,11 +2,16 @@ import React, {useState} from 'react';
 import {TouchableOpacity} from 'react-native';
 import {TextInput} from 'react-native-paper';
 import {TextInputProps} from 'react-native-paper/lib/typescript/components/TextInput/TextInput';
-import DatePickerModal from '../modal/date-picker.modal';
+import DatePickerModal, {DatePickerPropsType} from '../modal/date-picker.modal';
 
 export type DatePickerInputPropsType = Partial<
   Omit<TextInputProps, 'editable'>
 > & {
+  /** Props for the date picker modal */
+  datePickerProps?: Omit<
+    DatePickerPropsType,
+    'onDismiss' | 'visible' | 'onPick'
+  >;
   /** Called when a date is picked through DatePickerModal */
   onPick?: (date: Date) => void;
 };
@@ -14,6 +19,7 @@ export type DatePickerInputPropsType = Partial<
 /** Provides a pressable TextInput based DatePicker */
 export default function DatePickerInput({
   onPick,
+  datePickerProps,
   ...props
 }: DatePickerInputPropsType) {
   const [show, setShow] = useState(false);
@@ -24,6 +30,7 @@ export default function DatePickerInput({
         <TextInput value={date} {...props} editable={false} />
       </TouchableOpacity>
       <DatePickerModal
+        {...datePickerProps}
         onDismiss={() => setShow(false)}
         onPick={date => {
           const dateOfBirth = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
