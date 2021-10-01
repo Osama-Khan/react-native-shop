bfc3ff4d1f5c1af87ccf0f64dee173a21c551b9d
Osama Khan <Osama6756@gmail.com>
2021-10-01 12:51:33 +0500

    Added SocketService

---
+++ src/app/services/socket.service.ts
@@ -0,0 +1,46 @@
+import {io} from 'socket.io-client';
+import store from '../store';
+import {UserState} from '../store/state';
+import ApiService from './api.service';
+
+/** Service to provide a single socket for the entire application */
+class SocketService extends ApiService {
+  socket = io(this.domain);
+  currentUser?: UserState;
+
+  constructor() {
+    super();
+    store.subscribe(this.onUpdate);
+  }
+
+  private onUpdate = () => {
+    const user = store.getState().user;
+    if (user.token) {
+      const userChanged = this.currentUser && user.id !== this.currentUser.id;
+      if (userChanged) {
+        // Refresh the socket since user has changed
+        this.socket = io(super.domain);
+      }
+      if (!this.currentUser || userChanged) {
+        this.currentUser = user;
+        this.doPostLogin();
+      }
+    }
+  };
+
+  private doPostLogin = () => {
+    this.socket.emit('login', this.currentUser!.id);
+    this.socket.on('receiveMessage', ({message, sender, time}) => {
+      store.dispatch({
+        type: 'message/receive',
+        payload: {
+          sender,
+          message,
+          time,
+        },
+      });
+    });
+  };
+}
+
+export default new SocketService();
