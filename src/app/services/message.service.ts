a439af091825c20c3f9f0b6e59e4f71c9ad68b79
Osama Khan <Osama6756@gmail.com>
2021-10-02 14:35:00 +0500

    Added sendMessage method to MessageService

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -2,15 +2,24 @@ import axios from 'axios';
 import Criteria from '../models/criteria';
 import {MessageType} from '../models/types/message.type';
 import ApiService from './api.service';
+import socketService from './socket.service';
 
 class MessageService extends ApiService {
   endpoint = this.domain + '/messages';
 
-  async fetchMessages(criteria?: Criteria<MessageType>) {
+  fetchMessages = async (criteria?: Criteria<MessageType>) => {
     const critStr = criteria?.getUrlParameters() || '';
     const ret = await axios.get(this.endpoint + critStr);
     return ret;
-  }
+  };
+
+  /** Emits sendMessage event to the socket
+   * @param message Message to emit
+   * @param to ID of the thread to send message to
+   */
+  sendMessage = (message: string, to: string) => {
+    socketService.socket.emit('sendMessage', message, to);
+  };
 }
 
 export default new MessageService();
