b90fe3c9cc71d5ce95a7a23e0410c4991073c85a
Osama Khan <Osama6756@gmail.com>
2021-10-03 13:52:26 +0500

    Improved NewMessageType

--- src/app/store/state/message-state.ts
+++ src/app/store/state/message-state.ts
@@ -1,12 +1,15 @@
-type NewMessageType = {
-  /** ID of the user who sent the message */
-  sender: number;
-  /** Content of the message */
-  message: string;
-  /** Time the message was sent at */
-  time: Date;
-  /** ID of the thread the message belongs to */
-  threadId: number;
+import {MessageType} from '@app/models/types/message.type';
+import {ThreadType} from '@app/models/types/thread.type';
+import {UserType} from '@app/models/types/user.types';
+
+/** Type that contains an optimized version of Message relations to reduce
+ * data usage
+ */
+export type NewMessageType = MessageType & {
+  /** Object with ID of the user who sent the message */
+  sender: Pick<UserType, 'id'>;
+  /** Object with ID of the thread the message was sent to */
+  thread: Pick<ThreadType, 'id'>;
 };
 
 export default class MessageState {
