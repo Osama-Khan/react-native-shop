2b98f591d3b593a7fb92924bad45b9eab9c16b76
Osama Khan <Osama6756@gmail.com>
2021-10-05 13:31:52 +0500

    Imporved New Thread modal UI

--- src/app/chat/new-thread-modal.tsx
+++ src/app/chat/new-thread-modal.tsx
@@ -1,19 +1,14 @@
 import Modal from '../components/modal';
 import {ModalPropType} from '@app/components/modal/modal-prop-type';
 import React, {useState} from 'react';
-import {
-  Divider,
-  IconButton,
-  TextInput,
-  Card,
-  Title,
-  Text,
-} from 'react-native-paper';
-import {View} from 'react-native';
+import {Divider, IconButton, Card, Title, Text} from 'react-native-paper';
+import {View, TextInput} from 'react-native';
 import threadService from '../services/thread.service';
 import {ThreadType} from '@app/models/types/thread.type';
 import {UserType} from '@app/models/types/user.types';
 import {SelectUserModal} from './select-user-modal';
+import s from '../../styles/styles';
+import themeService from '../services/theme.service';
 
 type P = Omit<ModalPropType, 'children'> & {
   userId: number;
@@ -27,21 +22,29 @@ export const NewThreadModal = ({userId, visible, onDismiss, onSend}: P) => {
   return (
     <>
       <Modal visible={visible} onDismiss={onDismiss}>
-        <Title style={{margin: 8, fontWeight: 'bold'}}>New Chat</Title>
+        <Title style={[s.m8, s.textBold]}>New Chat</Title>
         <Divider />
-        <View style={{padding: 8}}>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Text>Send To: </Text>
-            <Card style={{padding: 8}} onPress={() => setPickerVisible(true)}>
+        <View style={s.p8}>
+          <View style={s.row}>
+            <Text style={s.alignCenter}>Send To: </Text>
+            <Card style={s.p8} onPress={() => setPickerVisible(true)}>
               <Text>{user ? user.username : 'Choose User'}</Text>
             </Card>
           </View>
           <TextInput
-            label="Message"
-            mode="outlined"
-            multiline
+            style={[
+              s.rounded,
+              s.p8,
+              {
+                borderWidth: 2,
+                borderColor: themeService.currentTheme.colors.border,
+                backgroundColor: themeService.currentTheme.colors.card,
+              },
+            ]}
             onChangeText={message => setMessage(message)}
-            disabled={loading}
+            placeholder={'Your Message...'}
+            editable={!loading}
+            multiline
           />
         </View>
         <Divider />
